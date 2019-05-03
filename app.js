4d056d2c4ccf3c4889545c3158bcde6cd52fc69d
Logan Yu <yu.logan@gmail.com>
2019-05-03 16:56:12 +0800

    configure production heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -13,6 +14,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
   
 app.use(passport.initialize());
 require('./config/passport')(passport);
